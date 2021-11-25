15487c5c83facb918a29e2ba65f8a6cf1ad4b9ec
delaircavalcante <delairc6@gmail.com>
2021-11-25 15:29:56 -0300

    modificações do Delair

--- src/pages/Logado/styles.ts
+++ src/pages/Logado/styles.ts
@@ -1,6 +1,22 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-export const Container = styled.div`
-    
-    
+export const Container = styled.main`
+
+  button {
+    display: flex;
+    flex-direction: row; 
+    align-items: center;
+  }
+  img {
+    display: flex;
+    flex: 1;
+
+    width: 2.2rem;
+    height: 2.2rem;
+
+    margin-left: 1.2rem;
+    margin-right: 1.2rem;
+  }
 `;
+
+export const Content = styled.div``;
