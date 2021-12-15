68f0b4cc7ad5f0fd7490b78a63f3fba0f2faec21
yurivenceslau <gamesclayman@gmail.com>
2021-12-15 16:37:37 -0300

    refatorando inputs e adicao de mascara.

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import React from "react"
 import { Dashboard } from "./pages/Questoes/index";
 import { Perfil } from "./pages/Perfil";
 import { Login } from "./pages/Login";
+import { Contato } from "./pages/Contato";
 
 
 
@@ -12,9 +13,10 @@ export function MainRoutes(){
          <Routes>
               {/* <Route path="/" element={<Login/>}/> */}
               <Route path="/login" element={<Login/>}/>
+              <Route path="/contato" element={<Contato/>}/>
               
-              <Route path="/" element={<Perfil/>}/>
-              <Route path="/dashboard" element={<Dashboard/>}/>
+              {/* <Route path="/" element={<Perfil/>}/>
+              <Route path="/dashboard" element={<Dashboard/>}/> */}
               {/* <Route path="/Perfil" element={<Perfil/>}/> */}
               {/* <Route path="*" element={<h1>Not Found</h1>}/> */}
          </Routes>
